d0167db1862efeb4e8a9ad4262cd10e485548785
oneone <2452561324@qq.com>
2024-07-12 01:45:35 +0800

    homework

---
+++ 32code/2.adc/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '2.adc' 
+ * Target:  '2.adc' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
